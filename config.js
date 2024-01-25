595032c7ff2d8bb359167f568f64f441640a76cd
faixt2013 <157658932+faixt2013@users.noreply.github.com>
2024-01-25 03:25:17 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  token: "MTE2ODI2NTk1MTU0NDg3MzEwMQ.GiAavU.pfcfKwuPyXeItjc5OBj7Lr-pZY7KcO4gAUCbBo",
+  /**
+    online
+    idle
+    dnd
+    invisible
+  */
+  status: "idle",
+  activity: {
+    name: "اكتب حالتك هنا",
+    /**
+      Playing = 0,
+      Streaming = 1,
+      Listening = 2,
+      Watching = 3,
+      Competing = 5
+    */
+    type: 1,
+  },
+};
---
+++ start_with_data_prv.bat
@@ -0,0 +1,3 @@
+node . --niro --prv
+
+pause
